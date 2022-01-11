0b19075eff59de3067d4adeafb431a6c466dc70a
MiniGarreto <82673060+MiniGarreto@users.noreply.github.com>
2022-01-11 15:38:40 +0100

    Update hachage.java

--- hachage.java
+++ hachage.java
@@ -1,12 +1,22 @@
+import java.math.BigInteger;
+
 public class HTNaive {
 
-	private ListeBigI[] T;
+    private ListeBigI[] T;
 
-	public HTNaive(int m) {
-		T = new ListeBigI[m];
-	}
+    public HTNaive(int m) {
+        this.T = new ListeBigI[m];
+    }
 
-	public ListeBigI getliste(int i) {
-		return t[i];
-	}
+    public ListeBigI getliste(int i) {
+        return this.T[i];
+    }
+
+    public int h(BigInteger u) {
+        return u.intValue() % this.T.length;
+    }
+
+    public boolean contient(BigInteger u) {
+        int indice = this.T[h(u)].contient 
+    }
 }
